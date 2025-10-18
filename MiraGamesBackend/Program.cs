006cbfe53b6093c7d84c339fdfe79e8a8475ca64
Kunjut <tembulats2020@gmail.com>
2025-10-19 02:05:04 +0500

    Начальная загрузка проекта

---
+++ MiraGamesBackend/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.OpenApi.Models;
+
+namespace MiraGamesBackend
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Добавление поддержки контроллеров и представлений
+            builder.Services.AddControllersWithViews();
+
+            // Подключение сервисов для Swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "API Тествого задания Mira Games",
+                    Version = "v1",
+                    Description = "Backend-сервис, который собирает, агрегирует и предоставляет данные о будущих релизах игр на ноябрь из Steam"
+                });
+
+                options.EnableAnnotations();
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseSwagger(); // Добавление Swagger
+            app.UseSwaggerUI(); // Добавление пользовательского интерфейса Swagger
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.Run();
+        }
+    }
+}
